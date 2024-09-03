b218e708d929ca5da22d61729a29072d3f907fbe
unknown <gsdmfsencion@gmail.com>
2024-09-03 22:40:03 +0200

    base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  publicPath: 'https://GSencion.github.io/Portafolio-Gilbert-Sencion',
+  base: 'https://GSencion.github.io/Portafolio-Gilbert-Sencion',
 })
